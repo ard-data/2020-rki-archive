d69e90a50c10931d25676aa4fb9250a3b369669f
Michael Kreil <github@michael-kreil.de>
2020-05-27 14:00:24 +0200

    fix non-GNU xargs error

--- bin/2_deduplicate.sh
+++ bin/2_deduplicate.sh
@@ -6,4 +6,4 @@ cd "../data/0_archived/"
 
 echo "deduplicate"
 
-md5sum *.bz2 | sort | awk 'BEGIN{lasthash = ""} $1 == lasthash {print $2} {lasthash = $1}' | xargs -r rm
+md5sum *.bz2 | sort | awk 'BEGIN{lasthash = ""} $1 == lasthash {print $2} {lasthash = $1}' | xargs rm 2> /dev/null || true
