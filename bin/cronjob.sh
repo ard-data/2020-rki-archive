cba42371086dfe539fa74f76c9349854b696a30f
Michael Kreil <github@michael-kreil.de>
2021-01-19 19:40:47 +0100

    Update cronjob.sh

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -8,14 +8,24 @@ cd "$(dirname "$0")"
 
 git pull
 
-node 1_download.js
-sh 2_deduplicate.sh
-node 3_parse.js
+signalNoUpdate=0
+signalUpdate=42
+
+{
+	node 1_download.js &&
+	sh 2_deduplicate.sh &&
+	node 3_parse.js
+} || {
+	error=$?
+	signalNoUpdate=$error
+	signalUpdate=$error
+	echo "ERROR happend $error"
+}
 
 sleep 3
 
 git add ../data/
-git commit -m "automatic data update" || exit 0
+git commit -m "automatic data update" || exit $signalNoUpdate
 git push
 
-exit 42
+exit $signalUpdate
