257f577abffcd6c710bc4852e99a8e52bdd1e40d
Michael Kreil <github@michael-kreil.de>
2021-02-01 13:48:22 +0100

    don't delete data in bucket

--- bin/6_upload.sh
+++ bin/6_upload.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")"
 
 echo "upload"
 
-gsutil rsync -c -C -d -e -x '(?!.*\.(xz|txt|html))' ../data/0_archived/ gs://brdata-public-data/rki-corona-archiv/0_archived/ 2>&1
-gsutil rsync -c -C -d -e -x '(?!.*\.(xz|txt|html))' ../data/1_ignored/ gs://brdata-public-data/rki-corona-archiv/1_ignored/ 2>&1
-gsutil rsync -c -C -d -e -x '(?!.*\.(xz|txt|html))' ../data/2_parsed/ gs://brdata-public-data/rki-corona-archiv/2_parsed/ 2>&1
+gsutil rsync -c -C -e -x '(?!.*\.(xz|txt|html))' ../data/0_archived/ gs://brdata-public-data/rki-corona-archiv/0_archived/ 2>&1
+gsutil rsync -c -C -e -x '(?!.*\.(xz|txt|html))' ../data/1_ignored/ gs://brdata-public-data/rki-corona-archiv/1_ignored/ 2>&1
+gsutil rsync -c -C -e -x '(?!.*\.(xz|txt|html))' ../data/2_parsed/ gs://brdata-public-data/rki-corona-archiv/2_parsed/ 2>&1
