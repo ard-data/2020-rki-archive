81d866216743bfcf772304dc63b5ebd25f333f48
Michael Kreil <github@michael-kreil.de>
2021-03-01 17:28:48 +0100

    log each run

--- bin/loop.sh
+++ bin/loop.sh
@@ -4,6 +4,8 @@ cd "$(dirname "$0")"
 
 while true
 do
+	echo "start run at $(date '+%Y-%m-%d %H:%M:%S')"
 	/home/michaelkreil/projects/notificato/notificato.sh "Corona Scraper" ./run.sh || true
+	echo "finished run at $(date '+%Y-%m-%d %H:%M:%S')"
 	sleep 600
 done
