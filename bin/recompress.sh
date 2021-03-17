d91c300692395f0e7fa33ee9503718787441179f
Michael Kreil <github@michael-kreil.de>
2021-03-17 18:06:21 +0100

    Create recompress.sh

---
+++ bin/recompress.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "helper script to recompress files in place"
+
+set -e
+
+bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+tmp_dir="$( cd "$( dirname "$bin_dir" )"/tmp && pwd )"
+src_dir="$( pwd )"
+dst_dir="$( pwd )"
+
+for filename in "$@"
+do
+	src_file="$src_dir"/"$filename"
+	tmp_file="$tmp_dir"/"$filename"
+	dst_file="$dst_dir"/"$filename"
+
+	echo "recompress $filename"
+
+	xz -dkcq $src_file | xz -z9eq > $tmp_file
+	mv -f $tmp_file $dst_file
+done
