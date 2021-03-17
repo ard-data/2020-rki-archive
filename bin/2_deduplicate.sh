2e3b44a0f01f0420de0c56f064c88c863763f9f4
Michael Kreil <github@michael-kreil.de>
2021-03-17 17:38:11 +0100

    only 2021

--- bin/2_deduplicate.sh
+++ bin/2_deduplicate.sh
@@ -6,4 +6,4 @@ cd "../data/0_archived/"
 
 echo "deduplicate"
 
-md5sum *.xz | sort | awk 'BEGIN{lasthash = ""} $1 == lasthash {print $2} {lasthash = $1}' | xargs rm 2> /dev/null || true
+md5sum 2021*.xz | sort | awk 'BEGIN{lasthash = ""} $1 == lasthash {print $2} {lasthash = $1}' | xargs rm 2> /dev/null || true
