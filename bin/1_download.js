b783ce525fa7b40babab38d329d06a126e939151
Michael Kreil <github@michael-kreil.de>
2021-03-18 13:31:59 +0100

    every 10 minutes, but ignore, when data has not changed

--- bin/1_download.js
+++ bin/1_download.js
@@ -22,7 +22,7 @@ const tempFolder = resolve(__dirname,'../tmp/');
 		fs.renameSync(filenameTmp, filenameOut);
 	} else {
 		// no new download
-		// process.exit(42);
+		if (Math.random() < 0.9) process.exit(42);
 	}
 })()
 
--- bin/loop.sh
+++ bin/loop.sh
@@ -7,5 +7,5 @@ do
 	echo "start run at $(date '+%Y-%m-%d %H:%M:%S')"
 	/home/michaelkreil/projects/notificato/notificato.sh "Corona Scraper" ./run.sh || true
 	echo "finished run at $(date '+%Y-%m-%d %H:%M:%S')"
-	sleep 1800
+	sleep 600
 done
