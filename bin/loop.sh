fe83ba6090059352398812d781adcbd4b9243add
Michael Kreil <github@michael-kreil.de>
2021-11-09 13:33:59 +0100

    print duration as time

--- bin/loop.sh
+++ bin/loop.sh
@@ -4,11 +4,15 @@ cd "$(dirname "$0")"
 
 while true; do
 	echo "start run at $(date '+%Y-%m-%d %H:%M:%S')"
+	SECONDS=0
+
 	if [ -f "/home/michaelkreil/projects/notificato/notificato.sh" ]; then
 		/home/michaelkreil/projects/notificato/notificato.sh "Corona Scraper" ./run.sh || true
 	else
 		./run.sh || true
 	fi
-	echo "finished run at $(date '+%Y-%m-%d %H:%M:%S')"
+
+	printf "   took %02d:%02d:%02d\n" "$((SECONDS / 3600 % 24))" "$((SECONDS / 60))" "$((SECONDS % 60))"
+
 	sleep 600
 done
