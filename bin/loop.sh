d6e588f329c42841fe63a90419e0038e2ede3d55
Michael Kreil <github@michael-kreil.de>
2022-03-07 14:20:16 +0100

    fix home path

--- bin/loop.sh
+++ bin/loop.sh
@@ -6,8 +6,8 @@ while true; do
 	echo "start run at $(date '+%Y-%m-%d %H:%M:%S')"
 	SECONDS=0
 
-	if [ -f "/home/michaelkreil/projects/notificato/notificato.js" ]; then
-		node /home/michaelkreil/projects/notificato/notificato.js "Corona Scraper" ./run.sh || true
+	if [ -f "~/projects/notificato/notificato.js" ]; then
+		node ~/projects/notificato/notificato.js "Corona Scraper" ./run.sh || true
 	else
 		./run.sh || true
 	fi
